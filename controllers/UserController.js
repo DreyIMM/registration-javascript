83f8f82a27f0c49d9a62d0b0d1699e7196c9dc38
DreyIMM <andreydario26@gmail.com>
2022-01-11 21:02:59 -0300

    Incluindo validaçao no formulario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -62,8 +62,20 @@ class UserController {
     getValues() {
         //o let user faz essa variavel existir somente aqui
         let user = {};
+        let isValid = true;
+
         console.log(typeof this.formEl.elements);
         [...this.formEl.elements].forEach(function (field, index) {
+
+            if(['name', 'email', 'password'].indexOf(field.name) >-1 && !field.value){
+
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+                
+            }
+
+
+
             if (field.name == "gender") {
                 if (field.checked) {
                     user[field.name] = field.value;
@@ -75,6 +87,10 @@ class UserController {
                 //user[name] = o valor do field
             }
         });
+
+        if(!isValid){
+            return false;
+        }
 
         return new User(
             user.name,
